b7f492ad7d63f43fc8070735a2f58cb1b471285f
donymagrony <daniyal.mambetov.04@gmail.com>
2024-10-06 02:41:17 +0500

    Pre last vers

--- myproject/blog/forms.py
+++ myproject/blog/forms.py
@@ -5,8 +5,14 @@ class NewPostForm(forms.ModelForm):
     class Meta:
         model = Post  # Link this form to the Post model
         fields = ['title', 'content']  # Specify the fields you want in the form
+        widgets = {
+            'content': forms.Textarea(attrs={'cols': 30, 'rows': 4})  # Smaller size
+        }
         
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['content']
+        widgets = {
+            'content': forms.Textarea(attrs={'cols':30,'rows':4})
+        }
--- myproject/blog/views.py
+++ myproject/blog/views.py
@@ -5,8 +5,7 @@ from .models import Post,Comment
 from .forms import CommentForm, NewPostForm
 from .serializers import PostSerializer
 from django.contrib.auth.mixins import LoginRequiredMixin
-
-
+from django.core.paginator import Paginator
 
 class BasePostView(LoginRequiredMixin, View):
     model = Post  # Base model for post views
@@ -19,29 +18,53 @@ class BasePostView(LoginRequiredMixin, View):
     def get_post(self, pk):
         """Fetch a single post by primary key."""
         return get_object_or_404(self.model, pk=pk)
+    
 class PostsListCreateView(BasePostView):
     template_name = 'post_list.html'
+    paginate_by = 1
     
+    def get_posts(self, request):
+        title_query = request.GET.get('title', '')
+        author_query = request.GET.get('author', '')
+        user_posts = Post.objects.filter(author=request.user).order_by('title')
+        other_posts = Post.objects.exclude(author=request.user).order_by('title')
+
+        if title_query:
+            user_posts = user_posts.filter(title__icontains=title_query)
+            other_posts = other_posts.filter(title__icontains=title_query)
+
+        if author_query:
+            user_posts = user_posts.filter(author__username__icontains=author_query)
+            other_posts = other_posts.filter(author__username__icontains=author_query)
+
+        post_list = list(user_posts) + list(other_posts)
+        paginator = Paginator(post_list, self.paginate_by)
+        page_number = self.request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+        return page_obj
+
     def get(self, request):
-        posts = self.get_posts()
+        posts = self.get_posts(request)
         form = NewPostForm()
         context = {
             'posts': posts,
-            'form': form  # Include the form for creating new posts
+            'form': form ,
+            'page_obj':posts
         }
         return render(request, self.template_name, context)
-    
-    
+
     def post(self, request):
         form = NewPostForm(request.POST)
         if form.is_valid():
-            post = form.save(commit=False)  # Don't save to the database yet
-            post.author = request.user  # Assign the current user as the author
-            post.save()  # Now save it
-            return redirect('posts')  # After creating a post, redirect to the posts list
+            post = form.save(commit=False)  
+            post.author = request.user  
+            post.save()  
+            return redirect('posts')  
         context = {
-            'posts': self.get_posts(),
-            'form': form  # Re-render form with errors if invalid
+            'posts': self.get_posts(request),
+            'form': form  ,
+            'title_query': request.GET.get('title', ''),
+            'author_query': request.GET.get('author', '')
         }
         return render(request, self.template_name, context)
 
@@ -51,8 +74,8 @@ class PostDetailView(BasePostView):
 
     def get(self, request, pk):
         post = self.get_post(pk)
-        comments = post.comments.all()  # Fetch comments for the post
-        comment_form = CommentForm()  # Empty form for adding comments
+        comments = post.comments.all()  
+        comment_form = CommentForm()  
         context = {
             'post': post,
             'comments': comments,
